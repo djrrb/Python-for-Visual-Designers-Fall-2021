255f3bf2ccf920bbf824f72cd5c69db7152d58b2
David Ross <david@djr.com>
2021-10-21 23:52:17 -0400

    challenge solutions

---
+++ session-1/10_numericRange.py
@@ -0,0 +1,5 @@
+# get a range of numbers
+# this is an easy way to do a loop without having to loop through a string
+# sorry i didn’t show this during class, will definitely show it next time
+for index in range(20):
+    print(index)
