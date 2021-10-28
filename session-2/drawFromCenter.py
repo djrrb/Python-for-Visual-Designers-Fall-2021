cb2e161d1816d15a70bae0de1324e13fe4ae3ea1
David Ross <david@djr.com>
2021-10-28 19:16:12 -0400

    Create drawFromCenter.py

---
+++ session-2/drawFromCenter.py
@@ -0,0 +1,13 @@
+# move the canvas by half width and height
+translate(width()/2, height()/2)
+
+# rect(x, y, width, height)
+# set a size variable
+mySize = 200
+
+# draw a rectangle
+# subtracting half width and height of object for x and y
+rect(-mySize/2, -mySize/2, mySize, mySize)
+
+
+
